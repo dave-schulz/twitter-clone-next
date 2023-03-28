f6b2e6e4878e81eb7635d80c1b400dba5f1374ae
Davey Schulz <daveschulz@hotmail.nl>
2023-03-28 20:58:30 +0200

    Added the create a post functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
+import Form from '@/components/Form';
 import Header from '@/components/Header';
 
 const Home = () => {
   return (
     <>
       <Header label="home" />
+      <Form placeholder="What's happening?" />
+      <PostFeed />
     </>
   );
 };
